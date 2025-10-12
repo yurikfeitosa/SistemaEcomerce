45dc0824d05744ca0221ea10f8890c4bd56b6ae2
yurik <yurikfeitosa@gmail.com>
2025-10-12 11:36:50 -0300

    inicial

---
+++ E-commerce/src/main/java/com/mycompany/dao/ConexaoMySQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dao;
+
+/**
+ *
+ * @author yurik
+ */
+public class ConexaoMySQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
